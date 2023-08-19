5e0259bc2af1e82aee54284f6383febeeefa3ad7
YoVinchen <gzh298255@gmail.com>
2023-08-19 20:21:30 +0800

    整合基础框架

---
+++ auth-service/src/main/java/com/test/AuthApplication.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: AuthApplication
+ * Package: com.test
+ *
+ * @author yovinchen
+ * @Create 2023/8/19 20:14
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class,args);
+    }
+}
